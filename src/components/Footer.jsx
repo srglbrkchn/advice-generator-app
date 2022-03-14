bfe119f2dbd1f90b9ed6985a64157bbd59f26c43
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-03-14 15:01:46 +0100

    FInitial commit. Create backbone of the app.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+
+            <div class="attribution">
+                Challenge by
+                <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"> Frontend Mentor </a>. Coded by
+                <a href="#"> srglbrkchn </a>.
+            </div>
+        </div>
+
+    );
+}
+
+
+export default Footer;
